d1fa96621407c159d19f34dc84ff6bcca8e1af17
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-09 19:12:10 -0700

    Added initial MageChar concrete product class

---
+++ src/MageChar.cpp
@@ -0,0 +1,37 @@
+#ifndef __MAGE_CHAR_CPP__
+#define __MAGE_CHAR_CPP__
+
+#include "../header/MageChar.h"
+
+MageChar::MageChar() {
+    this->name = "Remy The Rat";
+    this->atk = 0; 
+    this->def = 0;
+    this->hp = 0; 
+
+}
+
+MageChar::MageChar(string name, double atk, double def, double hp) {
+    this->name = name;
+    this->atk = atk; 
+    this->def = def;
+    this->hp = hp; 
+}
+
+MageChar::MageChar() {
+
+}
+
+void MageChar::attack() {
+
+}
+
+void MageChar::defend() {
+
+}
+
+void MageChar::reduceHealth() {
+    
+}
+
+#endif 
