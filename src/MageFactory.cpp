e85321b0f6e95a7abc70e69164b40948bcf9de88
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-10 22:03:03 -0700

    Initial creation of MageFactory class

---
+++ src/MageFactory.cpp
@@ -0,0 +1,19 @@
+#ifndef __MAGE_FACTORY_CPP__
+#define __MAGE_FACTORY_CPP__
+
+#include "../header/MageFactory.h"
+#include "../header/MageChar.h"
+
+MageFactory::~MageFactory() {
+
+}
+
+MageFactory::~MageFactory() {
+
+}
+
+Character* MageFactory::createCharacter() {
+    return new MageChar();  
+}
+
+#endif
