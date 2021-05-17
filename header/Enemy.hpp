ddb80b86edd81699f80873c9444b5501f97de895
toothnnail <michael.s.dong@gmail.com>
2021-05-17 14:12:16 -0700

    Added the getter functions to the src files and updated names

--- header/Enemy.hpp
+++ header/Enemy.hpp
@@ -14,11 +14,12 @@ private:
 public:
    Enemy();
    Enemy(int); //creates enemy scaled to level (int)
-   double getHp(); //getter (hp)
-   double getAtk(); //getter (atk)
-   double getDef(); //getter (def)
+   string getName() const; //getter (name)
+   double getHp() const; //getter (hp)
+   double getAtk() const; //getter (atk)
+   double getDef() const; //getter (def)
    void attack(Character*); //will target a character and deal damage
-   void reduceHp(); //reduces hp
+   void reduceHealth(double); //reduces hp
 }
 
 #endif //__ENEMY_HPP__
--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -20,11 +20,19 @@ Enemy::Enemy(int levelNum) {
    this->def = (levelNum / 2.0) + 10.0;
 }
 
+string Enemy::getName() const { return this->name; }
+
+double Enemy::getHp() const { return this->hp; }
+
+double Enemy::getAtk() const { return this->atk; }
+
+double Enemy::getDef() const { return this->def; }
+
 void Enemy::attack(Character* target) {
-   target->reduceHp(this->atk);
+   target->reduceHealth(this->atk);
 }
 
-void Enemy::reduceHp(double damage) {
+void Enemy::reduceHealth(double damage) {
    if(this->def < damage) { //if there is not enough defense to negate all damage
    	this->hp = this->hp - (this->damage - this->def);
    }
