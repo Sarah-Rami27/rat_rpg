0dc4f491ac64c7c9286170736fa9c7c20d1d40b0
sarah-rami27 <sarrami270@gmail.com>
2021-05-27 19:47:41 -0700

    tests added for Game class

--- tests/game_test.hpp
+++ tests/game_test.hpp
@@ -1,8 +1,32 @@
+#define private public
 #ifndef _GAME_TEST_HPP_
 #define _GAME_TEST_HPP_
 
 #include "gtest/gtest.h"
 #include "../header/Game.h"
+
+
+TEST(GameTest, setFactory){
+    Game testGame;
+    testGame.setFactory(1);
+    EXPECT_TRUE(testGame.factory != nullptr);
+}
+
+TEST(GameTest, createPlayerWarr){
+   Game testGame;
+   testGame.setFactory(1);
+   testGame.createPlayer();
+   std::string name = testGame.player->getName();
+   EXPECT_EQ(name, "Remy The Rat");   //default name for warrior
+}
+
+TEST(GameTest, createPlayerMage){
+   Game testGame;
+   testGame.setFactory(2);
+   testGame.createPlayer();
+   std::string name = testGame.player->getName();
+   EXPECT_EQ(name, "Emile The Rat");   //default name for Mage
+}
 
 TEST(GameTest, isAliveTrueW){
     Game testGame;
