2eb2a1980995917e82fdde880df09358d3592dde
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-28 04:05:34 -0700

    Updated tests to scale with level

--- tests/weapon_decorator_test.hpp
+++ tests/weapon_decorator_test.hpp
@@ -10,15 +10,15 @@
 
 TEST(DecoratorTest, DmgIncrease) {
     Weapon* warWep = new WarriorWeapon(); 
-    Weapon* dmgInc = new DamageIncrease(warWep); 
-    EXPECT_EQ(dmgInc->calculateDamage(0, 0), 11);
+    Weapon* dmgInc = new DamageIncrease(warWep, 1); 
+    EXPECT_EQ(dmgInc->calculateDamage(0, 0), 3);
 }
 
 TEST(DecoratorTest, DmgIncreaseTwo) {
     Weapon* warWep = new WarriorWeapon(); 
-    Weapon* dmgInc = new DamageIncrease(warWep);
-    Weapon* dmgInc2 = new DamageIncrease(dmgInc); 
-    EXPECT_EQ(dmgInc2->calculateDamage(0, 0), 21);
+    Weapon* dmgInc = new DamageIncrease(warWep, 1);
+    Weapon* dmgInc2 = new DamageIncrease(dmgInc, 1); 
+    EXPECT_EQ(dmgInc2->calculateDamage(0, 0), 5);
 }
 
 
