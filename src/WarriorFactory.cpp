31230ab284dbe27e524e1b4d55abc292761a5b52
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-28 03:38:40 -0700

    Changed createdWeapon to use scale

--- src/WarriorFactory.cpp
+++ src/WarriorFactory.cpp
@@ -17,7 +17,7 @@ Character* WarriorFactory::createCharacter() {
 Weapon* WarriorFactory::createWeapon(int levelNum) {
     double minDamage = (levelNum / 3.00) + 1.00;
 	double maxDamage = (levelNum / 3.00) + 5.00;
-    return new WarriorWeapon(); 
+    return new WarriorWeapon(minDamage, maxDamage); 
 }
 
 Armor* WarriorFactory::createArmor() {
