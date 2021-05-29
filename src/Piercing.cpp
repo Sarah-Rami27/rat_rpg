52bf58a2ad443475084bef5126bb46221b2e50e9
Sarah Ramirez <sarrami270@gmail.com>
2021-05-29 00:00:54 -0700

    implemented calc Damage

---
+++ src/Piercing.cpp
@@ -0,0 +1,17 @@
+#ifndef _PIERCING_CPP_
+#define _PIERCING_CPP_
+
+#include "../header/Piercing.h"
+
+Piercing::Piercing(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum){}
+
+Piercing::~Piercing(){}
+
+double Piercing::calculateDamage(double playerAtk, double enemyDef){
+    RNG rng;
+    double result = rng.pickDouble(.01, .02 * (levelNum + 1));
+    double damage = enemyDef * (1 - result);
+    return damage + WeaponDecorator::calculateDamage(playerAtk, enemyDef);
+}
+
+#endif
