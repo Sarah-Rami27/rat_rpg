d46092968ecb813dbfbc942b9f3dfa529ff5b524
sarah-rami27 <sarrami270@gmail.com>
2021-05-28 20:32:57 -0700

    more mage tests

--- tests/character_tests.hpp
+++ tests/character_tests.hpp
@@ -6,7 +6,7 @@
 #include "../header/WarriorChar.h"
 
 
-/*constructors*/
+/*default constructors*/
 TEST(MageChar, defaultMage){
     Character* mage = new MageChar();
     std::string name = "Emile The Rat";
@@ -17,6 +17,18 @@ TEST(WarriorChar, defaultWarrior){
     Character* warrior = new WarriorChar();
     std::string name = "Remy The Rat";
     EXPECT_EQ(warrior->getName(), name);
+}
+/* Mage tests*/
+TEST(MageChar, mageAttack){
+    Character* mage = new MageChar();
+    Enemy* enemy = new Enemy(0);
+    mage->attack(enemy);
+    EXPECT_EQ(enemy->getHp(), 9.0);
+}
+TEST(MageChar, mageDefend){
+    Character* mage = new MageChar();
+    mage->defend();
+    EXPECT_EQ(mage->getDef(),3 );
 }
 
 #endif
