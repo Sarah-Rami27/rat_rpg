45ea5cd81084cbee73523c72240d1021a594d018
Sarah Ramirez <sarrami270@gmail.com>
2021-05-29 00:16:44 -0700

    values changed

--- src/Piercing.cpp
+++ src/Piercing.cpp
@@ -9,7 +9,7 @@ Piercing::~Piercing(){}
 
 double Piercing::calculateDamage(double playerAtk, double enemyDef){
     RNG rng;
-    double result = rng.pickDouble(.01, .02 * (levelNum + 1));
+    double result = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
     double damage = enemyDef * (1 - result);
     return damage + WeaponDecorator::calculateDamage(playerAtk, enemyDef);
 }
