1fcd9728ff2c3a0f8c7aa1909cad717aef7996e2
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-28 19:26:25 -0700

    Added CritChance perk

---
+++ src/CritChance.cpp
@@ -0,0 +1,26 @@
+#ifndef __CRIT_CHANCE_CPP__
+#define __CRIT_CHANCE_CPP__
+
+#include "../header/CritChance.h"
+
+CritChance::CritChance(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum) {}
+
+CritChance::~CritChance() {}
+
+double CritChance::calculateDamage(double playerAtk, double enemyDef) {
+    double critChance = 0.05; 
+    RNG rng;
+    while (levelNum > 0) {
+        critChance += rng.pickDouble(0.02, 0.04);
+        levelNum--; 
+    }
+    if(rng.roll(critChance)) {
+        return 2 * WeaponDecorator::calculateDamage(playerAtk, enemyDef); 
+    }
+    else {
+        return WeaponDecorator::calculateDamage(playerAtk, enemyDef);
+    }
+}
+
+
+#endif
