a2bdbd407a4776c6348b800720937f83a1c2a0ee
sarah-rami27 <sarrami270@gmail.com>
2021-05-29 18:00:35 -0700

    extra hit tests

--- tests/extra_hit_test.hpp
+++ tests/extra_hit_test.hpp
@@ -12,5 +12,11 @@ TEST(ExtraHitTest, oneExtraHit){
     EXPECT_DOUBLE_EQ(exHit->calculateDamage(0,0), 2);
 }
 
+TEST(ExtraHitTest, ExtraHit){
+    Weapon* test = new MageWeapon();
+    Weapon* exHit = new ExtraHit(test, 0);
+    Weapon* exHit2 = new ExtraHit(exHit, 0);
+    EXPECT_DOUBLE_EQ(exHit2->calculateDamage(0,0), 4);
+}
 
 #endif //_EXTRA_HIT_HPP_
