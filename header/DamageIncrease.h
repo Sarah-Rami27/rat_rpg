f47d72d773d39d7e332560dbf69bd86ec358e599
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-30 01:23:45 -0700

    moved extra damage calculation to constructor

--- header/DamageIncrease.h
+++ header/DamageIncrease.h
@@ -5,10 +5,13 @@
 #include "Rng.h"
 
 class DamageIncrease: public WeaponDecorator {
+    private: 
+        double extraDamage; 
     public:
         DamageIncrease(Weapon*, int); 
         ~DamageIncrease(); 
         double calculateDamage(double, double);  
+        void printStats(std::ostream&);
 };
 
 #endif 
