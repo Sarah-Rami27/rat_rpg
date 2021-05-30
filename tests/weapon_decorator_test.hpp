6af08abccc033d6c2b073bc2d9fe99ded7c3aa99
sarah-rami27 <sarrami270@gmail.com>
2021-05-30 14:54:46 -0700

    more tests

--- tests/weapon_decorator_test.hpp
+++ tests/weapon_decorator_test.hpp
@@ -35,7 +35,26 @@ TEST(DecoratorTest, fourPerks) {
     Weapon* perk2 = new DamageIncrease(perk1, 5);
     Weapon* perk3 = new ExtraHit(perk2, 5);
     Weapon* perk4 = new Piercing(perk3, 5);
-    EXPECT_TRUE(19 <= perk3->calculateDamage(0,0) <= 33 );
+    EXPECT_TRUE(19 <= perk4->calculateDamage(0,0) <= 33 );
+}
+
+TEST(DecoratorTest, fourPerks2) {
+    Weapon* test = new WarriorWeapon();
+    Weapon* perk4 = new Piercing(test, 5);
+    Weapon* perk1 = new CritChance(perk4, 5);
+    Weapon* perk2 = new DamageIncrease(perk1, 5);
+    Weapon* perk3 = new ExtraHit(perk2, 5);
+    EXPECT_TRUE(23.4 <= perk3->calculateDamage(1,5) <= 49.4 );
+}
+
+TEST(DecoratorTest, fivePerks) {
+    Weapon* test = new WarriorWeapon();
+    Weapon* perk1 = new CritChance(test, 5);
+    Weapon* perk2 = new DamageIncrease(perk1, 5);
+    Weapon* perk3 = new ExtraHit(perk2, 5);
+    Weapon* perk4 = new Piercing(perk3, 5);
+    Weapon* perk5 = new ExtraHit(perk4, 5);
+    EXPECT_TRUE(38 <= perk4->calculateDamage(0,0) <= 38);
 }
 
 
