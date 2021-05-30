894e195d700b14b16b7e9a5991e95ce3b3a33ec7
sarah-rami27 <sarrami270@gmail.com>
2021-05-30 01:20:19 -0700

    weapon tests

--- tests/weapon_test.hpp
+++ tests/weapon_test.hpp
@@ -1,3 +1,4 @@
+#define protected public
 #ifndef __WEAPON_TEST_HPP__
 #define __WEAPON_TEST_HPP__
 
@@ -7,14 +8,34 @@
 #include "../header/WarriorWeapon.h"
 #include "../header/MageWeapon.h"
 
+
+TEST(WarriorWeaponTest,defaultWeaponW){
+    Weapon* warWep = new WarriorWeapon();
+    EXPECT_EQ(warWep->minDamage, 1 );
+    EXPECT_EQ(warWep->maxDamage, 2);
+} 
+
+TEST(WarriorWeaponTest,constrWeaponW){
+    Weapon* warWep = new WarriorWeapon(1, 7);
+    EXPECT_EQ(warWep->minDamage, 1 );
+    EXPECT_EQ(warWep->maxDamage, 7);
+}
+
+TEST(MageWeaponTest,defaultWeaponM){
+    Weapon* magWep = new MageWeapon();
+    EXPECT_EQ(magWep->minDamage, 1 );
+    EXPECT_EQ(magWep->maxDamage, 2);
+}
+
+TEST(MageWeaponTest,constrWeaponM){
+    Weapon* magWep = new MageWeapon(1, 7);
+    EXPECT_EQ(magWep->minDamage, 1 );
+    EXPECT_EQ(magWep->maxDamage, 7);
+}
+
 TEST(WarriorWeaponTest, DefaultCalcDmg) {
     Weapon* warWep = new WarriorWeapon(); 
     EXPECT_TRUE(1 <= warWep->calculateDamage(0, 0) <= 2); 
-}
-
-TEST(WarriorWeaponTest, DefaultCalcLifeDrain) {
-    Weapon* warWep = new WarriorWeapon(); 
-    EXPECT_EQ(warWep->calculateLifeDrain(), 0); 
 }
 
 TEST(MageWeaponTest, DefaultCalcDmg) {
@@ -22,10 +43,14 @@ TEST(MageWeaponTest, DefaultCalcDmg) {
     EXPECT_TRUE(1 <= magWep->calculateDamage(0, 0) <= 2);
 }
 
-TEST(MageWeaponTest, DefaultCalcLifeDrain) {
-    Weapon* magWep = new MageWeapon(); 
-    EXPECT_EQ(magWep->calculateLifeDrain(), 0); 
+TEST(WarriorWeaponTest, WCalcDmg) {
+    Weapon* warWep = new WarriorWeapon();
+    EXPECT_TRUE(5 <= warWep->calculateDamage(4, 0) <= 6);
 }
 
+TEST(MageWeaponTest, MCalcDmg) {
+    Weapon* magWep = new MageWeapon();
+    EXPECT_TRUE(5 <= magWep->calculateDamage(4, 0) <= 6);
+}
 
 #endif
