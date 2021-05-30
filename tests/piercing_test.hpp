d555bea37bfda4f85d0bb408186c883d5c74ab42
sarah-rami27 <sarrami270@gmail.com>
2021-05-29 17:54:42 -0700

    piercing tests

--- tests/piercing_test.hpp
+++ tests/piercing_test.hpp
@@ -5,11 +5,24 @@
 #include "../header/WarriorWeapon.h"
 #include "../header/MageWeapon.h"
 
-TEST(PiercingTest, levelZero){
+TEST(PiercingTest, EnemyDefZero){
     Weapon* test = new WarriorWeapon();
     Weapon* pier = new Piercing(test, 0);
     EXPECT_EQ(pier->calculateDamage(0,0), 1);
 
+}
+
+TEST(PiercingTest, EnemyDefFive ){
+    Weapon* test = new WarriorWeapon();
+    Weapon* pier = new Piercing(test, 10);
+    EXPECT_TRUE(1.7 <= pier->calculateDamage(0,5) <= 3.35 );
+}
+
+TEST(PiercingTest, pierStack ){
+    Weapon* test = new WarriorWeapon();
+    Weapon* pier = new Piercing(test, 10);
+    Weapon* pier2 = new Piercing(pier, 10);
+    EXPECT_TRUE(3.4 <= pier->calculateDamage(0,5) <= 6.7 );
 }
 
 #endif
