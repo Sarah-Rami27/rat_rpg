59855f4daabd0dcce8ff013104059da12ccbdeab
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-30 01:24:30 -0700

    Moved piercing calculation to constructor

--- src/Piercing.cpp
+++ src/Piercing.cpp
@@ -3,15 +3,22 @@
 
 #include "../header/Piercing.h"
 
-Piercing::Piercing(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum){}
+Piercing::Piercing(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum){
+    RNG rng;
+    piercing = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
+}
 
 Piercing::~Piercing(){}
 
 double Piercing::calculateDamage(double playerAtk, double enemyDef){
-    RNG rng;
-    double result = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
-    double damage = enemyDef * (1 - result);
+    
+    double damage = enemyDef * (1 - piercing);
     return damage + WeaponDecorator::calculateDamage(playerAtk, enemyDef);
+}
+
+void Piercing::printStats(std::ostream& out) {
+    cout << "Piercing: " << (piercing * 100) << "%" << endl;
+    WeaponDecorator::printStats(out);
 }
 
 #endif
