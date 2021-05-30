b5c3824944ee26b5104538ea119d9b1ff76d2e65
toothnnail <michael.s.dong@gmail.com>
2021-05-30 01:18:34 -0700

    Changed to doubles

--- src/Piercing.cpp
+++ src/Piercing.cpp
@@ -10,8 +10,8 @@ Piercing::~Piercing(){}
 double Piercing::calculateDamage(double playerAtk, double enemyDef){
     RNG rng;
     double piercing = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
-    if(piercing > 1) { piercing = 1; }
-    double damage = enemyDef * (1 - piercing);
+    if(piercing > 1.0) { piercing = 1.0; }
+    double damage = enemyDef * (1.0 - piercing);
     return damage + WeaponDecorator::calculateDamage(playerAtk, enemyDef);
 }
 
