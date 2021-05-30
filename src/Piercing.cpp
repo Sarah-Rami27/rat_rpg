9e441eaade0c63885af7a792a6f38dbd31a879bc
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-30 02:29:56 -0700

    Fixed piercing again

--- src/Piercing.cpp
+++ src/Piercing.cpp
@@ -5,7 +5,7 @@
 
 Piercing::Piercing(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum){
     RNG rng;
-    double piercing = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
+    piercing = rng.pickDouble(.03 * (levelNum + 1), .06 * (levelNum + 1));
     if(piercing > 1.0) { 
         piercing = 1.0;
     }
