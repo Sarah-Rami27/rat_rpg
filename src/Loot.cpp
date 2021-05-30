b00647dd7bb219f9a71b28595755db6d912ed6cc
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-29 23:11:27 -0700

    Removed rng object definition

--- src/Loot.cpp
+++ src/Loot.cpp
@@ -9,7 +9,6 @@ Weapon* Loot::spawnWeapon(int levelNum, ClassTypeFactory* factory) {
 }
 
 Armor* Loot::spawnArmor(int levelNum, ClassTypeFactory* factory) {	
-	RNG rng; 
 	double def = rng.pickDouble(levelNum * 0.5, levelNum * 1.5);
 	return factory->createArmor(levelNum + def);
 }
