d567420a4e9dc84c027739e44e1141e4b4cbca75
sarah-rami27 <sarrami270@gmail.com>
2021-05-30 03:07:07 -0700

    tests added

--- tests/weapon_decorator_test.hpp
+++ tests/weapon_decorator_test.hpp
@@ -7,15 +7,35 @@
 #include "../header/MageWeapon.h"
 #include "../header/WeaponDecorator.hpp"
 #include "../header/DamageIncrease.h"
+#include "../header/ExtraHit.h"
+#include "../header/CritChance.h"
+#include "../header/Piercing.h"
 
 // Integration testing for decorator
 // i.e combination of different perks 
 
-TEST(DecoratorTest, test1) {
+TEST(DecoratorTest, twoPerks) {
+    Weapon* test = new WarriorWeapon();
+    Weapon* perk1 = new DamageIncrease(test, 1);
+    Weapon* perk2 = new ExtraHit(perk1, 1 );
+    EXPECT_TRUE(5 <= perk2->calculateDamage(0,0) <= 9);
 }
 
-TEST(DecoratorTest, test2) {
+TEST(DecoratorTest, threePerks) {
+    Weapon* test = new WarriorWeapon();
+    Weapon* perk1 = new CritChance(test, 5);
+    Weapon* perk2 = new DamageIncrease(perk1, 5); 
+    Weapon* perk3 = new ExtraHit(perk2, 5);
+    EXPECT_TRUE(19 <= perk3->calculateDamage(0,0) <= 33 );   
+}
 
+TEST(DecoratorTest, fourPerks) {
+    Weapon* test = new WarriorWeapon();
+    Weapon* perk1 = new CritChance(test, 5);
+    Weapon* perk2 = new DamageIncrease(perk1, 5);
+    Weapon* perk3 = new ExtraHit(perk2, 5);
+    Weapon* perk4 = new Piercing(perk3, 5);
+    EXPECT_TRUE(19 <= perk3->calculateDamage(0,0) <= 33 );
 }
 
 
