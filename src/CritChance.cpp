31e904e0ebd35878dbefa93f1fd3f33e5ee14100
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-05-30 01:22:29 -0700

    Created critChance member variable and moved calculation to constructor

--- src/CritChance.cpp
+++ src/CritChance.cpp
@@ -3,23 +3,28 @@
 
 #include "../header/CritChance.h"
 
-CritChance::CritChance(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum) {}
+CritChance::CritChance(Weapon* weapon, int levelNum): WeaponDecorator(weapon, levelNum) {
+    while (levelNum > 0) {
+        critChance += rng.pickDouble(0.01, 0.02);
+        levelNum--; 
+    }
+}
 
 CritChance::~CritChance() {}
 
 double CritChance::calculateDamage(double playerAtk, double enemyDef) {
-    double critChance = 0.05; 
     RNG rng;
-    while (levelNum > 0) {
-        critChance += rng.pickDouble(0.02, 0.04);
-        levelNum--; 
-    }
     if(rng.roll(critChance, 1)) {
         return 2 * WeaponDecorator::calculateDamage(playerAtk, enemyDef); 
     }
     else {
         return WeaponDecorator::calculateDamage(playerAtk, enemyDef);
     }
+}
+
+void CritChance::printStats(std::ostream& out) {
+    cout << "Crit Chance: " << (critChance * 100) << "%" << endl;
+    WeaponDecorator::printStats(out);
 }
 
 
